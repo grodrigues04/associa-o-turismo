81ef4c7b77dc7d13f7c48d0423cfb11da17098db
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-10-20 11:28:08 -0300

    feat: add associacao page

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from home.views import home_view, associacao_view, contato_view
+from . import views
+
+urlpatterns = [
+    path('', home_view),
+    path('associação/', associacao_view),
+    path("contato", contato_view)
+]
