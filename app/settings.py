1f80489991684fc2f6722d4e903dc66c69ba613f
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-10-20 16:11:58 -0300

    buid: add whitenoise middleware

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n2v75pd28c!ji793u20txq5diq-2c_$_&pb_$()*6s%!w*zkuv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
